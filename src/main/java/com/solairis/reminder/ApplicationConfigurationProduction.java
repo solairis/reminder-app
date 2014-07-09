35cec8f6cf574ef479b720f140b21f1b191a8440
Joshua P Johnson <joshjohnson@trulia.com>
2014-07-09 13:35:28 -0700

    Move twilio account sid and auth token out of the application.

--- src/main/java/com/solairis/reminder/ApplicationConfigurationProduction.java
+++ src/main/java/com/solairis/reminder/ApplicationConfigurationProduction.java
@@ -3,9 +3,11 @@ package com.solairis.reminder;
 
 import com.twilio.sdk.TwilioRestClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
 
 /**
  *
@@ -13,10 +15,17 @@ import org.springframework.context.annotation.Profile;
  */
 @Configuration
 @Profile("production")
+@PropertySource("file:application.properties")
 public class ApplicationConfigurationProduction {
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Value("${twilio.auth.token}")
+	private String twilioAuthToken;
+	
+	@Value("${twilio.account.sid}")
+	private String twilioAccountSid;
 	
 	@Bean
 	public ReminderRepository getReminderRepository() {
@@ -25,12 +34,12 @@ public class ApplicationConfigurationProduction {
 	
 	@Bean
 	public String getTwilioAccountSid() {
-		return "ACf7476379d7c599528fb70fd0803b0aae";
+		return this.twilioAccountSid;
 	}
 	
 	@Bean
 	public String getTwilioAuthToken() {
-		return "961c034a0f44cc26495a899df675ee3a";
+		return this.twilioAuthToken;
 	}
 	
 	@Bean
